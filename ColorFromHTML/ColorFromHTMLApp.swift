bd982c0b38c9ba604395050e10fb0a3ac4fe5e16
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2024-01-22 11:01:21 +0300

    Initial Commit

---
+++ ColorFromHTML/ColorFromHTMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorFromHTMLApp.swift
+//  ColorFromHTML
+//
+//  Created by MIKHAIL ZHACHKO on 22.01.24.
+//
+
+import SwiftUI
+
+@main
+struct ColorFromHTMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorFromHTML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorFromHTML
+//
+//  Created by MIKHAIL ZHACHKO on 22.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
